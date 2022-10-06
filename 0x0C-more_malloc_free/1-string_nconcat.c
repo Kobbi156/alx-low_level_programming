18c947b0e4a853db22778bb5aa281f6096a0f03c
Kobbi156 <henryagyeman156@gmail.com>
2022-10-06 14:58:30 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,50 @@
-#include <string.h>
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two string
+ * string_nconcat - function that concatenates two strings
+ *
  * @s1: string 1
  * @s2: string 2
- * @n: number of bytes to copy
+ * @n: bytes of @s2 to add to @s1 to be a new string
  *
- * Return: pointer to new string
- */
+ * Return: new string followed by the first @n bytes
+ *         of string 2 @s2 or NULL
+*/
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
+	unsigned int l1, l2, i, j;
 	char *str;
-	unsigned int l1;
-	unsigned int l2;
 
+	/*treat NULL as empty string*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	l1 = strlen(s1);
-	l2 = strlen(s2);
+
+	l1 = l2 = 0;
+	/*length of both s1 & s2 respectively*/
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
+
+	/*set n to length of s2*/
 	if (n >= l2)
 		n = l2;
-	str = malloc((l1 + l2 + 1) * sizeof(char));
+
+	str = (char *) malloc((l1 + n + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < l1; i++)
-	{
+
+	/*add s1 to str*/
+	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
-	}
-	for (; i < l1 + n; i++)
+	/*add s2 n bytes to str*/
+	for (j = 0; j < n && s2[j] != '\0'; j++)
 	{
-		str[i] = s2[i - l1];
+		str[i] = s2[j];
+		i++;
 	}
 	str[i] = '\0';
 	return (str);
