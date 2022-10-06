401faa0e619680a372cfe8670764c2c6b5e2cfb9
Kobbi156 <henryagyeman156@gmail.com>
2022-10-06 15:00:30 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,28 +1,51 @@
-
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * _calloc - calloc
- * @nmemb: number of elements
- * @size: size in bytes
+ * _memset - fills memory with a constant byte
  *
- * Return: void pointer
- */
+ * @s: input pointer that represents memory
+ *     block to fill
+ * @b: characters to fill
+ * @n: number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory area
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * _calloc - a function that allocates
+ *           memory for an array using malloc
+ *
+ *           It is basically the equivalent to
+ *           malloc followed by memset
+ *
+ * @nmemb: size of array
+ * @size: size of each element
+ *
+ * Return: pointer with new allocated memory
+ *         or NULL if it fails
+*/
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *m;
-	unsigned int i= 0;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	m = malloc(nmemb * size);
-	if (m == NULL)
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
-	while (i < nmemb * size)
-	{
-		*(m + i) = 0;
-		i++;
-	}
-	return ((void *) m);
+
+	_memset(p, 0, nmemb * size);
+
+	return (p);
 }
